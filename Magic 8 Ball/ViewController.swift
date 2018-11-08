dec4669dde7674f9d10e8779592ca0d7a8a9a799
KARTIK SONI <sonikartik98@outlook.com>
2018-11-09 01:30:14 +0530

    Ball Image Updating Properly on click

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -10,11 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var ballImage: UIImageView!
+    var randomBallIndex: Int = 0
+    let ballArray = ["ball1","ball2","ball3","ball4","ball5"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func askButtonAtion(_ sender: UIButton) {
+        updateBallStatus()
+    }
+    
+    
+    
+    func updateBallStatus() {
+        randomBallIndex = Int.random(in: 0 ... 4)
+        ballImage.image = UIImage(named: ballArray[randomBallIndex])
+    }
+    
 }
-
