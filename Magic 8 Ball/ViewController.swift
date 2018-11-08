a64967f1a1dd5d3d6fb8c9e5cc619a1a16ea7f4a
KARTIK SONI <sonikartik98@outlook.com>
2018-11-09 00:50:08 +0530

    Initial Commit Project Created

---
+++ Magic 8 Ball/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Magic 8 Ball
+//
+//  Created by KARTIK SONI on 09/11/18.
+//  Copyright © 2018 KARTIK SONI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
